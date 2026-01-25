99a785e42742855977558ded7883ce80f4978d1a
RR <rrn091301@gmail.com>
2026-01-25 14:46:23 +0800

    feat: add styles on large screen sizes and group-hover style to categories component

--- src/components/shared/Categories.tsx
+++ src/components/shared/Categories.tsx
@@ -7,7 +7,7 @@ export default function Categories({ onClose }: { onClose?: () => void }) {
 		<div className="px-6 md:px-10">
 			<ContentWrapper>
 				<nav>
-					<ul className="grid h-min justify-items-center gap-4 md:grid-cols-3 md:gap-2.5">
+					<ul className="grid h-min justify-items-center gap-4 md:grid-cols-3 md:gap-2.5 lg:gap-7.5">
 						<Category onClose={onClose} category="headphones" />
 						<Category onClose={onClose} category="speakers" />
 						<Category onClose={onClose} category="earphones" />
@@ -25,28 +25,30 @@ interface CategoryProps {
 
 function Category({ category, onClose }: CategoryProps) {
 	return (
-		<li className="relative grid w-full justify-items-center">
-			<div>
-				<img
-					className="h-26"
-					src={`/assets/shared/desktop/image-category-thumbnail-${category}.png`}
-					alt={`${category} category thumbnail`}
-				/>
-			</div>
-			<h2 className="category mb-4">{category}</h2>
+		<li className="relative w-full">
 			<Link
 				to={`/category/${category}`}
 				onClick={onClose}
-				className="mb-5.5 flex items-center gap-3.5"
+				className="group grid justify-items-center"
 			>
-				<span className="subtitle hover:text-primary opacity-50 hover:opacity-100">
-					Shop
-				</span>
-				<span>
-					<img src={arrowRightIcon} alt="Arrow right icon" />
-				</span>
+				<div>
+					<img
+						className="h-26 lg:h-40"
+						src={`/assets/shared/desktop/image-category-thumbnail-${category}.png`}
+						alt={`${category} category thumbnail`}
+					/>
+				</div>
+				<h2 className="category lg:heading-6 mb-4">{category}</h2>
+				<p className="mb-5.5 flex items-center gap-3.5 lg:mb-7.5">
+					<span className="subtitle group-hover:text-primary opacity-50 group-hover:opacity-100">
+						Shop
+					</span>
+					<span>
+						<img src={arrowRightIcon} alt="Arrow right icon" />
+					</span>
+				</p>
+				<div className="bg-light-700 absolute bottom-0 left-0 -z-10 h-41 w-full rounded-lg lg:h-51"></div>
 			</Link>
-			<div className="bg-light-700 absolute bottom-0 left-0 -z-10 h-41 w-full rounded-lg"></div>
 		</li>
 	);
 }
