792a612eac805fe176baa019884b9725ad93175c
RR <rrn091301@gmail.com>
2026-02-06 18:53:53 +0800

    fix: move paddings from go back button to its parent container so going back only works when pressing the button itself and not on its paddings

--- src/components/GoBackButton.tsx
+++ src/components/GoBackButton.tsx
@@ -5,10 +5,10 @@ export default function GoBackButton() {
 	const navigate = useNavigate();
 
 	return (
-		<ContentWrapper className="w-full px-6 md:px-10">
+		<ContentWrapper className="w-full px-6 pt-4 pb-6 md:px-10 md:pt-8 lg:pt-20 lg:pb-14">
 			<button
 				onClick={() => navigate(-1)}
-				className="hover:text-primary cursor-pointer pt-4 pb-6 opacity-50 hover:opacity-100 md:pt-8 lg:pt-20 lg:pb-14"
+				className="hover:text-primary cursor-pointer opacity-50 hover:opacity-100"
 			>
 				Go Back
 			</button>
